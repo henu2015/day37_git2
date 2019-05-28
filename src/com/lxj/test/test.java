68adcc7db67d0599933e4cb0696d27389671e7b0
henu2015 <48999596+henu2015@users.noreply.github.com>
2019-05-28 11:47:11 +0800

    hgkjjafdskj

--- src/com/lxj/test/test.java
+++ src/com/lxj/test/test.java
@@ -3,6 +3,8 @@ package com.lxj.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
-        //System.out.println("hahhahey");
+        System.out.println("error");
+        System.out.println("success");
+        System.out.println("hahhahey");
     }
 }
