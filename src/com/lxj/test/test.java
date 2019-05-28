0e247a1a478378c2ce49c02aeaaa5ea019aaa20f
lxj.cy <lxj.cy@foxmail.com>
2019-05-28 11:48:43 +0800

    change new info

--- src/com/lxj/test/test.java
+++ src/com/lxj/test/test.java
@@ -2,7 +2,8 @@ package com.lxj.test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        //System.out.println("hahhahey");
+        System.out.println("hello");
+        //System.out.println("hjdafkdsa");
+        int a = 10;
     }
 }
