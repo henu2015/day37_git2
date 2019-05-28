56a3c3f55aafd44ba416e674265a24196a6893fb
lxj.cy <lxj.cy@foxmail.com>
2019-05-28 14:30:42 +0800

    hahkjhfkjdslahk

--- src/com/lxj/test/test.java
+++ src/com/lxj/test/test.java
@@ -2,12 +2,12 @@ package com.lxj.test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("S他人呢");
         //System.out.println("hjdafkdsa");
         int a = 10;
         System.out.println("hello world");
         //System.out.println("error");
-        System.out.println("success");
+        //System.out.println("success");
         System.out.println("hahhahey");
     }
 }
