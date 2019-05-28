57ed0da1769fa8759c240256f7cab4a2340c333c
lxj.cy <lxj.cy@foxmail.com>
2019-05-28 11:08:55 +0800

    add info

--- src/com/lxj/test/test.java
+++ src/com/lxj/test/test.java
@@ -3,5 +3,6 @@ package com.lxj.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hahhahey");
     }
 }
